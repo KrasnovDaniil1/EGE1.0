b4538b78740185b9f23893926db8b58dc6ade59d
KrasnovDaniil1 <kd69931@gmail.com>
2026-08-29 15:59:25 +0200

    test

--- regular/7600.py
+++ regular/7600.py
@@ -3,9 +3,7 @@ from re import *
 with open("7600.txt", "r") as f:
     data = f.readline()
 
-data = "QRS*QRQR*QSQS*RSRS"
-
-pattern = r''
+pattern = r'[^QRS]*([QRS][^QRS]+)+[QRS]?'
 matches = [m.group() for m in finditer(pattern, data)]
 
 answer = max(matches, key=len)
